0b172db927ff8976df22f415c2823a4b1f14da6b
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-10-04 00:56:01 +0900

    Update POST /api/v1/album

---
+++ src/models/Album.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import User from "./User";
+import Image from "./Image";
+
+const albumSchema = new mongoose.Schema({
+    by: {
+        type: mongoose.SchemaTypes.ObjectId,
+        required: true,
+        ref: User.modelName
+    },
+    title: {
+        type: String,
+        required: [true, "앨범 제목을 입력해주세요."]
+    },
+    songTitles: {
+        type: [String],
+        required: true
+    },
+    songs: {
+        type: [String],
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ["single", "album"],
+        required: true
+    },
+    image: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: Image.modelName
+    }
+});
+
+export interface IAlbum {
+    by: mongoose.Types.ObjectId,
+    title: string,
+    songTitles: string[],
+    songs: string[],
+    type: "single" | "album"
+    image?: mongoose.Types.ObjectId
+}
+
+export interface AlbumDocument extends mongoose.Document, IAlbum {}
+
+const Album = mongoose.model<AlbumDocument>("Album", albumSchema);
+
+export default Album;
--- src/routes/album.ts
+++ src/routes/album.ts
@@ -1,8 +1,66 @@
 import {Router, routes} from "../lib/decorators/router";
 import {Request, Response} from "express";
+import fs from 'fs';
+import authMiddleware from "../lib/middlewares/auth";
+import upload from "../lib/declarations/upload";
+import {BadRequestError} from "../lib/declarations/error";
+import Album from "../models/Album";
+import {isValidObjectId} from "mongoose";
 
 @Router
 class AlbumRoutes {
+    @routes('post', '/api/v1/album', {
+        middlewares: [authMiddleware(), upload.array('songs')]
+    })
+    async postAlbum(req: Request, res: Response) {
+        try {
+            const {albumName, kind, title, albumImageId} = req.body;
+            if (!albumName || !kind || (kind === "album" && !title) || !req.files || !req.files.length || !Array.isArray(req.files) || !isValidObjectId(albumImageId)) throw BadRequestError;
+
+            for (const file of req.files) {
+                if (!file.mimetype.match(/audio\/*/)) throw BadRequestError;
+            }
+
+            const album = new Album();
+            album.title = albumName;
+            album.type = kind;
+            album.image = albumImageId;
+            album.by = res.locals.user.id;
+
+            if (kind === "single") {
+                if (req.files.length > 1) throw BadRequestError;
+                album.songs = [
+                    req.files[0].filename
+                ];
+                album.songTitles = [
+                    albumName as string
+                ]
+            } else if (kind === "album") {
+                if (req.files.length !== title.length) throw BadRequestError;
+                album.songs = req.files.map(file => file.filename);
+                album.songTitles = title;
+            } else {
+                throw BadRequestError;
+            }
+
+            await album.save();
+
+            res.json({
+                success: true,
+                data: {
+                    albumId: album._id
+                }
+            });
+        } catch (e) {
+            const files = Array.isArray(req.files) ? req.files : Object.values(req.files);
+
+            for (const file of files) {
+                fs.unlinkSync((file as Express.Multer.File).path);
+            }
+
+            throw e;
+        }
+    }
     @routes('get', '/api/v1/album')
     async hello(req: Request, res: Response) {
         res.json({
