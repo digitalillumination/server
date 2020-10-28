cf934d2aaea52ca867756d8620541860ce0d3446
Doyeong Kim <48087967+kimdoyeong@users.noreply.github.com>
2020-10-28 13:08:02 +0900

    Update cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import applyRouters from "./lib/applyRouters";
 import routers from './routes';
 import errorRequestHandler from "./lib/errorRequestHandler";
 const app = express();
 
+app.use(cors({
+    origin: ["https://theil.doyeong.dev", "http://theil.doyeong.dev"]
+}))
 app.use(express.json());
 applyRouters(app, routers);
 
